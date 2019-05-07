638a7eee9b16badb0d09ec7fba71a6674976f70b
Michael Ulz <ulzmic14@htlkaindorf.at>
2019-05-07 20:58:39 +0200

    modified chartjsp & TmlnServlet

--- TMLN/src/java/Servlets/TmlnServlet.java
+++ TMLN/src/java/Servlets/TmlnServlet.java
@@ -5,8 +5,13 @@
  */
 package Servlets;
 
+import de.umass.lastfm.Artist;
+import de.umass.lastfm.Chart;
+import de.umass.lastfm.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.util.Collection;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,6 +37,7 @@ public class TmlnServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
         request.getRequestDispatcher("chartjsp.jsp").forward(request, response);
     }
 
@@ -61,7 +67,19 @@ public class TmlnServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String key = "4d2f280d1bdd14ca03f7383532c38d7f";
+        String user = "iMichi8";
+        Chart<Artist> chart = User.getWeeklyArtistChart(user, 10, key);
+        DateFormat format = DateFormat.getDateInstance();
+        String from = format.format(chart.getFrom());
+        String to = format.format(chart.getTo());
+        System.out.printf("Charts for %s for the week from %s to %s:%n", user, from, to);
+        Collection<Artist> artists = chart.getEntries();
+        request.setAttribute("artists", artists);
+        for (Artist artist : artists) {
+            System.out.println(artist.getName());
+        }
+        request.getRequestDispatcher("chartjsp.jsp").forward(request, response);
     }
 
     /**
--- TMLN/web/chartjsp.jsp
+++ TMLN/web/chartjsp.jsp
@@ -3,7 +3,6 @@
     Created on : 06.05.2019, 19:31:12
     Author     : michi
 --%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -13,5 +12,8 @@
     </head>
     <body>
         <h1>TMLN</h1>
+        <form action="TmlnServlet" method="POST">
+            <input type="submit" value="submit" />
+        </form>
     </body>
 </html>
